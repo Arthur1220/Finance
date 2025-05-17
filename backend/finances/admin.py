70d4399fd8043b3a18665f470a5b16e9065c5312
Arthur <arthurmarquesazevedo@gmail.com>
2025-05-17 10:42:37 -0300

    Registra modelos 'Category', 'Transaction' e 'Goal' no admin do Django com exibição e filtros apropriados.

--- backend/finances/admin.py
+++ backend/finances/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Category, Transaction, Goal
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'name', 'type')
+    list_filter  = ('type',)
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'category', 'amount', 'timestamp')
+
+@admin.register(Goal)
+class GoalAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'user', 'name', 
+        'target_amount', 'start_date', 
+        'end_date', 'frequency'
+    )
+    list_filter  = ('frequency',)
